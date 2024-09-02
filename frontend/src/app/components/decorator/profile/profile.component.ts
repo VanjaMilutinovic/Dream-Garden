4779f269ad96aec6acbf327ba82f8afbf5d5ce89
AD\npetric <nikola.petric@yettel.rs>
2024-09-02 23:24:17 +0200

    dodate promjene slike i promijenjene granice da 100 i 300 px slike mogu da se uploadaju

--- frontend/src/app/components/decorator/profile/profile.component.ts
+++ frontend/src/app/components/decorator/profile/profile.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AdminService } from 'src/app/services/admin/admin.service';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { UserService } from 'src/app/services/user/user.service';
-
 
 @Component({
   selector: 'app-profile',
@@ -15,18 +14,78 @@ export class ProfileComponent {
 
   constructor(private adminService: AdminService,
     private userService: UserService,
-    public sanitizer: DomSanitizer){}
+    public sanitizer: DomSanitizer) {}
 
   errorMsg: any;
   currentUser: User = new User();
   cardType: string | null = null;
   cardTypeIcon: string | null = null;
-
+  previewUrl: SafeUrl | null = null;
+  imageError?: string;
+  base64Image?: string | null = null;
+  profilePictureInvalid?: boolean = false;
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
   async ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('user') || '{}');
+    if (this.currentUser.photoId && this.currentUser.photoId.base64) {
+      this.previewUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.currentUser.photoId.base64);
+    }
   }
 
-  async editUser(){ 
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const img = new Image();
+
+      img.src = URL.createObjectURL(file);
+
+      img.onload = () => {
+        const width = img.width;
+        const height = img.height;
+
+        if (this.isImageDimensionInvalid(width, height)) {
+          this.imageError = 'Slika mora biti dimenzija 100x100 do 300x300 piksela.';
+          this.profilePictureInvalid = true;
+        } else {
+          this.imageError = '';
+          this.profilePictureInvalid = false;
+
+          // Set the preview URL
+          this.previewUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file));
+
+          const reader = new FileReader();
+          reader.onload = () => {
+            this.base64Image = (reader.result as string);
+          };
+          reader.readAsDataURL(file);
+        }
+      };
+      img.onerror = () => {
+        this.imageError = 'Nepravilan format slike.';
+        this.profilePictureInvalid = true;
+        this.previewUrl = null;
+        this.base64Image = null;
+      };
+    } else {
+      this.profilePictureInvalid = false;
+      this.previewUrl = null;
+      this.base64Image = null;
+    }
+  }
+
+  isImageDimensionInvalid(width: number, height: number): boolean {
+    const minDimension = 99;
+    const maxDimension = 301;
+    return (width < minDimension || height < minDimension || width > maxDimension || height > maxDimension);
+  }
+
+  async editUser() {
+    if (this.profilePictureInvalid) {
+      this.errorMsg = 'Molimo vas da unesete ispravnu sliku.';
+      return;
+    }
+
     let data = {
       userId: this.currentUser.userId,
       email: this.currentUser.email,
@@ -35,8 +94,25 @@ export class ProfileComponent {
       address: this.currentUser.address,
       contactNumber: this.currentUser.contactNumber,
       creditCardNumber: this.currentUser.creditCardNumber,
-      base64: this.currentUser.photoId.base64
+      base64: this.base64Image || this.currentUser.photoId.base64 // Use the new image if uploaded
+    };
+
+      try {
+      this.currentUser = await firstValueFrom(this.userService.update(data)) as User;
+      localStorage.setItem('user', JSON.stringify(this.currentUser));
+      this.successMessage = 'Profil je uspešno ažuriran';
+      this.errorMessage = null; // Clear any previous error message
+      setTimeout(() => {
+        this.successMessage = null;
+      }
+      , 3000);
+    } catch (error) {
+      this.errorMessage = 'Došlo je do greške prilikom ažuriranja korisničkih podataka.';
+      this.successMessage = null; // Clear any previous success message
+      setTimeout(() => {
+        this.errorMessage = null;
+      }
+      , 3000);
     }
-    this.currentUser = await firstValueFrom(this.userService.update(data)) as User;
   }
 }
--- frontend/src/app/components/owner/profile/profile.component.ts
+++ frontend/src/app/components/owner/profile/profile.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AdminService } from 'src/app/services/admin/admin.service';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { UserService } from 'src/app/services/user/user.service';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { set } from 'date-fns';
 
 @Component({
   selector: 'app-profile',
@@ -16,27 +15,84 @@ export class ProfileComponent {
 
   constructor(private adminService: AdminService,
     private userService: UserService,
-    public sanitizer: DomSanitizer){}
+    public sanitizer: DomSanitizer) {}
 
   errorMsg: any;
   currentUser: User = new User();
   cardType: string | null = null;
   cardTypeIcon: string | null = null;
+  previewUrl: SafeUrl | null = null;
+  imageError?: string;
+  base64Image?: string | null = null;
+  profilePictureInvalid?: boolean = false;
+  successMessage: string | null = null;  // New property for success message
+  errorMessage: string | null = null;    // New property for error message
 
   async ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('user') || '{}');
+    if (this.currentUser.photoId && this.currentUser.photoId.base64) {
+      this.previewUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.currentUser.photoId.base64);
+    }
     this.detectCardType();
+  }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const img = new Image();
+
+      img.src = URL.createObjectURL(file);
+
+      img.onload = () => {
+        const width = img.width;
+        const height = img.height;
+
+        if (this.isImageDimensionInvalid(width, height)) {
+          this.imageError = 'Slika mora biti dimenzija 100x100 do 300x300 piksela.';
+          this.profilePictureInvalid = true;
+        } else {
+          this.imageError = '';
+          this.profilePictureInvalid = false;
+
+          // Set the preview URL
+          this.previewUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file));
+
+          const reader = new FileReader();
+          reader.onload = () => {
+            this.base64Image = (reader.result as string);
+          };
+          reader.readAsDataURL(file);
+        }
+      };
+      img.onerror = () => {
+        this.imageError = 'Nepravilan format slike.';
+        this.profilePictureInvalid = true;
+        this.previewUrl = null;
+        this.base64Image = null;
+      };
+    } else {
+      this.profilePictureInvalid = false;
+      this.previewUrl = null;
+      this.base64Image = null;
+    }
+  }
+
+  isImageDimensionInvalid(width: number, height: number): boolean {
+    const minDimension = 99;
+    const maxDimension = 301;
+    return (width < minDimension || height < minDimension || width > maxDimension || height > maxDimension);
   }
 
   async editUser(){ 
     if (this.currentUser.creditCardNumber=='' || this.currentUser.creditCardNumber==null){
-      alert('Broj kreditne kartice je obavezan');
+      this.errorMessage = 'Broj kreditne kartice je obavezan';
       return;
     }
-    if (this.creditCardValidator()==false){
-      alert('Broj kreditne kartice nije validan');
+    if (!this.creditCardValidator()){
+      this.errorMessage = 'Broj kreditne kartice nije validan';
       return;
     }
+
     let data = {
       userId: this.currentUser.userId,
       email: this.currentUser.email,
@@ -45,11 +101,26 @@ export class ProfileComponent {
       address: this.currentUser.address,
       contactNumber: this.currentUser.contactNumber,
       creditCardNumber: this.currentUser.creditCardNumber,
-      base64: this.currentUser.photoId.base64
+      base64: this.base64Image || this.currentUser.photoId.base64
     }
-    this.currentUser = await firstValueFrom(this.userService.update(data)) as User;
-    localStorage.removeItem('user');
-    localStorage.setItem('user', JSON.stringify(this.currentUser));
+    
+    try {
+      this.currentUser = await firstValueFrom(this.userService.update(data)) as User;
+      localStorage.setItem('user', JSON.stringify(this.currentUser));
+      this.successMessage = 'Profil je uspešno ažuriran';
+      this.errorMessage = null; // Clear any previous error message
+      setTimeout(() => {
+        this.successMessage = null;
+      }
+      , 3000);
+    } catch (error) {
+      this.errorMessage = 'Došlo je do greške prilikom ažuriranja korisničkih podataka.';
+      this.successMessage = null; // Clear any previous success message
+      setTimeout(() => {
+        this.errorMessage = null;
+      }
+      , 3000);
+    }
   }
 
   creditCardValidator(): boolean {
@@ -57,12 +128,7 @@ export class ProfileComponent {
     const mastercard = /^(51|52|53|54|55)\d{14}$/;
     const visa = /^(4539|4556|4916|4532|4929|4485|4716)\d{12}$/;
     const creditCardNumber = this.currentUser.creditCardNumber;
-    // Check if the credit card number matches any of the regexes
-    if (diners.test(creditCardNumber) || mastercard.test(creditCardNumber) || visa.test(creditCardNumber)) {
-        return true;
-      }
-    return false;
-    
+    return diners.test(creditCardNumber) || mastercard.test(creditCardNumber) || visa.test(creditCardNumber);
   }
 
   detectCardType() {
